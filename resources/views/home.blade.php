f7603a9d517e9c9ac06f7731cbb970cfdec3b1d7
unknown <kencoding1031@gmail.com>
2023-08-31 15:58:34 +0900

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://camo.githubusercontent.com/03adfa4cac70588a30717c515df0c1e8976bcc339e00848ac214502637544e93/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f66726565636f646563616d702f637572726963756c756d2d6469616772616d2d66756c6c2e6a7067" style="max-height: 150px;" class="rounded-circle">
+        </div>
+        <div class="col-9 p-5">
+            <div><h1>freecodecamp</h1></div>
+            <div class= "d-flex">
+                <div style="padding-right: 40px;"><strong>153</strong> posts</div>
+                <div style="padding-right: 40px;"><strong>212</strong> following</div>
+                <div style="padding-right: 40px;"><strong>23k</strong> followers</div>
+            </div>
+            <div class="pt-4 fw-bold">freeCodeCamp.org</div>
+            <div>We're a global community of millions of people learning to code together. We're an open source, donor-supported, 501(c) (3)nonprofit.</div>
+            <div><a href="#">www.freecodecamp.org</a></div>
+        </div>
+    </div>
+
+    <div class="row pt-5">
+        <div class="col-4">
+            <img src="https://media.macphun.com/img/uploads/customer/how-to/579/15531840725c93b5489d84e9.43781620.jpg?q=85&w=1340" class="w-100" style="height:450px" alt="">
+        </div>
+        <div class="col-4">
+            <img src="https://media.macphun.com/img/uploads/customer/how-to/579/15531840725c93b5489d84e9.43781620.jpg?q=85&w=1340" class="w-100" style="height:450px" alt="">
+        </div>
+        <div class="col-4">
+            <img src="https://media.macphun.com/img/uploads/customer/how-to/579/15531840725c93b5489d84e9.43781620.jpg?q=85&w=1340" class="w-100" style="height:450px" alt="">
+        </div>
+    </div>
+</div>
+@endsection
